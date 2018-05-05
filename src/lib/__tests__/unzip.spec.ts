a49d4fadbfa43ea5ac2b42aa30965060295361b4
Emmanuel Gautier <git@emmanuelgautier.fr>
2018-05-05 18:53:48 +0200

    feat(unzip): Unzip Linkedin export file

---
+++ src/lib/__tests__/unzip.spec.ts
@@ -0,0 +1,34 @@
+import { beforeEach, test } from 'ava';
+import { readFile } from 'fs';
+import { resolve as resolvePath } from 'path';
+
+import { unzipFile } from '../unzip';
+
+let data: Buffer;
+beforeEach(() =>
+  new Promise((resolve, reject) => {
+    readFile(
+      resolvePath(__dirname, '../../../../test/ref/LinkedinDataExport.zip'),
+      (err: Error, dataBuffer: Buffer) => {
+        if (err) {
+          return reject(err);
+        }
+
+        data = dataBuffer;
+
+        resolve();
+      }
+    );
+  }));
+
+test('Return the right list of files', async t => {
+  const exportFiles = await unzipFile(data);
+
+  t.snapshot(exportFiles);
+});
+
+test('Return the right list of files when filter argument is passed', async t => {
+  const exportFiles = await unzipFile(data, ['educations', 'organizations']);
+
+  t.snapshot(exportFiles);
+});
---
+++ src/lib/unzip.ts
@@ -0,0 +1,60 @@
+import { loadAsync } from 'jszip';
+import { fromPairs, unzip, zip } from 'lodash';
+
+export interface ExportFiles {
+  causesYouCareAbout: string;
+  certifications: string;
+  educations: string;
+  emails: string;
+  languages: string;
+  organizations: string;
+  positions: string;
+  profile: string;
+  projects: string;
+  skills: string;
+  testScores: string;
+}
+
+const filesName: Array<[string, string]> = [
+  ['Causes You Care About.csv', 'causesYouCareAbout'],
+  ['Certifications.csv', 'cerfifications'],
+  ['Education.csv', 'educations'],
+  ['Email Addresses.csv', 'emails'],
+  ['Languages.csv', 'languages'],
+  ['Organizations.csv', 'organizations'],
+  ['Positions.csv', 'positions'],
+  ['Profile.csv', 'profile'],
+  ['Projects.csv', 'projects'],
+  ['Skills.csv', 'skills'],
+  ['Test Scores.csv', 'testScores']
+];
+
+/**
+ * Unzip Linkedin ZIP Export file.
+ *
+ * @param data Buffer
+ * @param filter string[]
+ * @returns Promise
+ */
+export async function unzipFile(
+  data: Buffer,
+  filter: string[] = []
+): Promise<Partial<ExportFiles>> {
+  const { files } = await loadAsync(data);
+  const existingFilesName = Object.keys(files);
+
+  const [filesNameToLoad, filesPropertiesName] = unzip(
+    filesName.filter(
+      ([fileName, property]) =>
+        existingFilesName.includes(fileName) &&
+        (filter.length === 0 || filter.includes(property))
+    )
+  );
+
+  const filesToLoad = filesNameToLoad.map(fileName =>
+    files[fileName].async('text')
+  );
+  const filesContent = await Promise.all(filesToLoad);
+
+  return fromPairs(zip(filesPropertiesName, filesContent));
+}
