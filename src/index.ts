3d6760d35678729f7de877460b6e46f8c3affec0
Emmanuel Gautier <git@emmanuelgautier.fr>
2020-06-14 17:02:06 +0200

    fix: export interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 export { LinkedinImport, linkedinImport as default } from './lib/import';
+
+export { CauseYouCareAbout } from './lib/mappers/causesYouCareAbout';
+export { Certification } from './lib/mappers/certifications';
+export { Education } from './lib/mappers/educations';
+export { Email } from './lib/mappers/emails';
+export { Language } from './lib/mappers/languages';
+export { Organization } from './lib/mappers/organizations';
+export { Position } from './lib/mappers/positions';
+export { Profile } from './lib/mappers/profile';
+export { Project } from './lib/mappers/projects';
+export { Skill } from './lib/mappers/skills';
+export { TestScore } from './lib/mappers/testScores';
